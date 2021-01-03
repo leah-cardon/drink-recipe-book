8b595ed48847d028cd38ca0041d5cf829eebaaf0
Leah Cardon <cardonleah@gmail.com>
2021-01-02 21:24:41 -0700

    Added ingredient and alcoholic/non-alcoholic fields to advanced search form

--- src/components/advancedSearch/advancedSearch.jsx
+++ src/components/advancedSearch/advancedSearch.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
 
-function AdvancedSearch ({ search, handleChange }) {
+function AdvancedSearch (props) {
   return (
-    <h2>advanced search form here</h2>
+    <form className='centered'>
+      <input type='text' placeholder='ingredient 1' />
+      <input type='text' placeholder='ingredient 2' />
+      <input type='text' placeholder='ingredient 3' />
+      <input type='text' placeholder='ingredient 4' />
+      <input type='text' placeholder='ingredient 5' />
+      <label>
+        <input type='radio' value='both' name='non/alcoholic' checked='checked' /> Alcoholic & non-alcoholic
+      </label>
+      <label>
+        <input type='radio' value='Alcoholic only' name='non/alcoholic' /> Alcoholic only
+      </label>
+      <label>
+        <input type='radio' value='Non-alcoholic only' name='non/alcoholic' /> Non-alcoholic only
+      </label>
+    </form>
   );
 };
 
