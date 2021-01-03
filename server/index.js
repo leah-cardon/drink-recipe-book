9b329eb39259e09e3fea11f0f3cca629d8244101
Leah Cardon <cardonleah@gmail.com>
2021-01-03 14:27:11 -0700

    Fixed eslint issues in advancedSearch module

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 const port = 3000;
 
@@ -8,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/profile', (req, res) => {
-  res.send('My profile page!')
+  res.send('My profile page!');
 });
 
 app.listen(port, () => {
--- src/components/advancedSearch/advancedSearch.jsx
+++ src/components/advancedSearch/advancedSearch.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
 
-
-function AdvancedSearch (props) {
+function AdvancedSearch(props) {
   return (
-    <form className='centered'>
-      <input type='text' placeholder='ingredient 1' />
-      <input type='text' placeholder='ingredient 2' />
-      <input type='text' placeholder='ingredient 3' />
-      <input type='text' placeholder='ingredient 4' />
-      <input type='text' placeholder='ingredient 5' />
-      <label>
-        <input type='radio' value='both' name='non/alcoholic' checked='checked' /> Alcoholic & non-alcoholic
+    <form className="centered">
+      <input type="text" placeholder="ingredient 1" />
+      <input type="text" placeholder="ingredient 2" />
+      <input type="text" placeholder="ingredient 3" />
+      <input type="text" placeholder="ingredient 4" />
+      <input type="text" placeholder="ingredient 5" />
+      <label htmlFor="both">
+        <input type="radio" value="both" id="both" name="non/alcoholic" checked="checked" />
+        Alcoholic & non-alcoholic
       </label>
-      <label>
-        <input type='radio' value='Alcoholic only' name='non/alcoholic' /> Alcoholic only
+      <label htmlFor="alcoholic">
+        <input type="radio" value="Alcoholic only" id="alcoholic" name="non/alcoholic" />
+        Alcoholic only
       </label>
-      <label>
-        <input type='radio' value='Non-alcoholic only' name='non/alcoholic' /> Non-alcoholic only
+      <label htmlFor="non-alcoholic">
+        <input type="radio" value="Non-alcoholic only" id="non-alcoholic" name="non/alcoholic" />
+        Non-alcoholic only
       </label>
-      <input type='submit' value='Search' />
+      <input type="submit" value="Search" />
     </form>
   );
-};
+}
 
 export default AdvancedSearch;
