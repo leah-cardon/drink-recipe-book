e5104c7d6b4d9c1a8ce23a9c76c50d054023fc3d
Leah Cardon <cardonleah@gmail.com>
2021-01-03 16:14:06 -0700

    Fixed eslint issues in result.jsx and added propTypes

--- src/components/result/result.jsx
+++ src/components/result/result.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function SearchResult ({ result }) {
-
+function SearchResult({ result }) {
   return (
-    <div className='result'>
-      <div className='thumbnailContainer'>
-        <img src={result.strDrinkThumb} className='thumbnail' />
+    <div className="result">
+      <div className="thumbnailContainer">
+        <img
+          src={result.strDrinkThumb}
+          alt={result.strDrink}
+          className="thumbnail"
+        />
       </div>
       <h1>{result.strDrink}</h1>
     </div>
   );
-
 }
+
+SearchResult.propTypes = {
+  result: PropTypes.shape.isRequired,
+};
 
 export default SearchResult;
