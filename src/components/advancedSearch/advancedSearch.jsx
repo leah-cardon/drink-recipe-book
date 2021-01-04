c65f0b09b4d302880a83beaad9055596f1beed6d
Leah Cardon <cardonleah@gmail.com>
2021-01-04 13:40:30 -0700

    Refactored advanced search form to use Formik and alert the correct form values

--- src/components/advancedSearch/advancedSearch.jsx
+++ src/components/advancedSearch/advancedSearch.jsx
@@ -1,28 +1,77 @@
 import React from 'react';
+import {
+  Formik,
+  Form,
+  Field,
+  ErrorMessage,
+} from 'formik';
+import PropTypes from 'prop-types';
 
 function AdvancedSearch({ advancedSearch }) {
   return (
-    <form className="centered">
-      <input type="text" placeholder="ingredient 1" />
-      <input type="text" placeholder="ingredient 2" />
-      <input type="text" placeholder="ingredient 3" />
-      <input type="text" placeholder="ingredient 4" />
-      <input type="text" placeholder="ingredient 5" />
-      <label htmlFor="both">
-        <input type="radio" value="both" id="both" name="non/alcoholic" checked="checked" />
-        Alcoholic & non-alcoholic
-      </label>
-      <label htmlFor="alcoholic">
-        <input type="radio" value="Alcoholic only" id="alcoholic" name="non/alcoholic" />
-        Alcoholic only
-      </label>
-      <label htmlFor="non-alcoholic">
-        <input type="radio" value="Non-alcoholic only" id="non-alcoholic" name="non/alcoholic" />
-        Non-alcoholic only
-      </label>
-      <input type="submit" value="Search" />
-    </form>
+    <div>
+      <Formik
+        initialValues={{
+          ingredient1: '',
+          ingredient2: '',
+          ingredient3: '',
+          ingredient4: '',
+          ingredient5: '',
+          'non/alcoholic': 'both',
+        }}
+        validate={(values) => {
+          const errors = {};
+          if (!values.ingredient1) {
+            errors.searchInput = 'Required';
+          }
+          // add error for if that ingredient is not contained in the db??
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            // search function here! send all form data
+            // search(values.searchInput);
+            alert(JSON.stringify(values));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form className="centered">
+            <Field type="text" name="ingredient1" />
+            <ErrorMessage name="ingredient1" component="div" />
+            <Field type="text" name="ingredient2" />
+            <ErrorMessage name="ingredient2" component="div" />
+            <Field type="text" name="ingredient3" />
+            <ErrorMessage name="ingredient3" component="div" />
+            <Field type="text" name="ingredient4" />
+            <ErrorMessage name="ingredient4" component="div" />
+            <Field type="text" name="ingredient5" />
+            <ErrorMessage name="ingredient5" component="div" />
+            <label>
+              <Field type="radio" value="both" name="non/alcoholic" checked="checked" />
+              Alcoholic & non-alcoholic
+            </label>
+            <label>
+              <Field type="radio" value="alcoholic" name="non/alcoholic" />
+              Alcoholic only
+            </label>
+            <label>
+              <Field type="radio" value="non-alcoholic" name="non/alcoholic" />
+              Non-alcoholic only
+            </label>
+            <button type="submit" disabled={isSubmitting}>
+              Search
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
 }
+
+AdvancedSearch.propTypes = {
+  advancedSearch: PropTypes.func.isRequired,
+};
 
 export default AdvancedSearch;
