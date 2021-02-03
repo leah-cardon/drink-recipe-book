29f965914345fc338cb2e3b90dde378d3484754e
Leah Cardon <cardonleah@gmail.com>
2021-02-03 00:17:51 -0700

    Moved index.html to root so that project can be deployed on GitHub pages easily

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, '/../src/dist')));
+app.use(express.static(path.join(__dirname, '/../')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
